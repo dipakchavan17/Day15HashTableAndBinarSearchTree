7cb4b1035e4c8da6e9703a61406956c8f4055a72
Dipak@1432 <chavandipakr17@gmail.com>
2022-02-06 22:14:17 +0530

    bst added

--- HashTableAndBinaryTree/HashTableAndBinaryTree/Program.cs
+++ HashTableAndBinaryTree/HashTableAndBinaryTree/Program.cs
@@ -11,31 +11,11 @@ namespace HashTableAndBinaryTree
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Hash Table And Binary Search Tree");
-            MyMapNode<string, string> hash = new MyMapNode<string, string>(20);
-            Console.WriteLine(hash.IsEmpty());
-            hash.Add("0", "paranoids");
-            hash.Add("1", "are");
-            hash.Add("2", "not");
-            hash.Add("3", "paranoid");
-            hash.Add("4", "because");
-            hash.Add("5", "they");
-            hash.Add("6", "are");
-            hash.Add("7", "paranoid");
-            hash.Add("8", "but");
-            hash.Add("9", "they");
-            hash.Add("10", "keep");
-            hash.Add("11", "putting");
-            hash.Add("12", "themselves");
-            hash.Add("13", "deliberately");
-            hash.Add("14", "into");
-            hash.Add("15", "paranoid");
-            hash.Add("16", "avoidable");
-            hash.Add("17", "situations");
-            Console.WriteLine(hash.GetV("5").GetHashCode());
-            Console.WriteLine(hash.GetV("0").GetHashCode());
-            Console.WriteLine($"size is: {hash.Getsize()}");
-            hash.Remove("16");
-            Console.WriteLine($"size is: {hash.Getsize()}");
+            Binary<int> bst = new Binary<int>(56);
+            bst.Insert(30);
+            bst.Insert(70);
+            Console.WriteLine("Binary Search Tree ");
+            bst.Display();
             Console.ReadLine();
         }
     }
